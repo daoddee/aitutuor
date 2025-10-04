59ce1e337b4d73aa16b08321a6077c7c23888ffb
daoddee <79615679+daoddee@users.noreply.github.com>
2025-10-04 13:58:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import AIMathsEngineeringFrontend from "@/components/AIMathsEngineeringFrontend";
+// app/page.tsx
+import AIMathsEngineeringFrontend from "../components/AIMathsEngineeringFrontend";
+// or, with named import since I added it:
+import { AIMathsEngineeringFrontend } from "../components/AIMathsEngineeringFrontend";
 
 export default function Page() {
   return <AIMathsEngineeringFrontend />;
